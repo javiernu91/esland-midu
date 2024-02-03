c968811c9e7207454aeb9d8ae857aa32e0f6fbb7
Javier Núñez <javiernu16@gmail.com>
2024-02-02 21:17:38 -0500

    Add i18n and the catalan page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,15 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es', //Idioma por defecto de la pagina
+    locales: ['es', 'ca'], //idiomas que soporta
+    //Enrutado
+    routing: {
+      prefixDefaultLocale: false //es -> /, ca -> /ca 
+    },
+
+  }
+
 });
